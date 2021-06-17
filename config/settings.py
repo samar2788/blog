079dde62e914bd36a4195d3efb29b1df90914dda
Samar Bhargava <abasive.sam@gmail.com>
2021-06-17 23:58:47 +0530

    Blog app updated

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #My app
     'blog',
+    'accounts',
     #Default django apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +128,6 @@ STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL = 'home'
